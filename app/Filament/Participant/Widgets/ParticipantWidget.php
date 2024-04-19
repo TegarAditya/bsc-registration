8f3dc609eeade014237fe45fb18e234db0fb8ed3
TegarAditya <tegaraditya53@gmail.com>
2024-04-19 14:44:43 +0700

    feat: adding Wa button

--- app/Filament/Participant/Widgets/ParticipantWidget.php
+++ app/Filament/Participant/Widgets/ParticipantWidget.php
@@ -12,4 +12,17 @@ class ParticipantWidget extends Widget
     {
         return 'full';
     }
+
+    public function getGroupLink(): string
+    {
+        if (in_array(auth()->user()->userDetail->grade, ['SD', 'MI'])) {
+            return "https://chat.whatsapp.com/BsbizO7HZkRBHh25qC01ey";
+        } else if (in_array(auth()->user()->userDetail->grade, ['SMP', 'MTs'])) {
+            return "https://chat.whatsapp.com/CHJWfDpxTG7Kfi2u0DSVX9";
+        } else if (in_array(auth()->user()->userDetail->grade, ['SMA', 'MA'])) {
+            return "https://chat.whatsapp.com/DIHH1jWGhpQ5MDI80AwodL";
+        }
+
+        return false;
+    }
 }
--- resources/views/filament/participant/widgets/participant-widget.blade.php
+++ resources/views/filament/participant/widgets/participant-widget.blade.php
@@ -3,7 +3,14 @@
         <div>
             <h1 class="text-xl font-semibold">Pendaftaran Berhasil</h1>
             <hr class="my-2">
-            <p class="text-sm">Selamat, Anda telah menjadi peserta Bupin Science Comptetition 2024</p>
+            <p class="text-sm">Selamat, Anda telah menjadi peserta Bupin Science Comptetition 2024.</p>
+            <p class="font-semibold text-sm">Silahkan bergabung ke grup WhatsApp dibawah ini.</p>
+        </div>
+        <br class="my-2">
+        <div>
+            <x-filament::button href="{{ $this->getGroupLink() }}" tag="a">
+                Group WhatsApp
+            </x-filament::button>
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
