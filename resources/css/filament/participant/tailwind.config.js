29405791e81cf7ba866148ba78221daab68bcced
TegarAditya <tegaraditya53@gmail.com>
2024-05-07 10:03:52 +0700

    custom theme

---
+++ resources/css/filament/participant/tailwind.config.js
@@ -0,0 +1,10 @@
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/Participant/**/*.php',
+        './resources/views/filament/participant/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+}
